bd2dc53b18f32302cf03d0bcd03803e22c87dc8d
DinDun <van.dinhdung.tkh@gmail.com>
2024-10-11 10:36:06 +0700

    CommitResources

---
+++ Assets/GameController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject[] UICanvas;
+    [SerializeField]
+    protected Button NextPanelButton;
+    [SerializeField]
+    protected Button PreviousButton;
+
+}
